080f664cccd798a0b539eab2ccf8d79f657cfebb
Michael McMillan <email@michaelmcmillan.net>
2016-11-30 15:47:09 +0100

    implemented header proxy

--- dist/stenograpi.py
+++ dist/stenograpi.py
@@ -38,6 +38,7 @@ class RequestBucket:
             return self.bucket.get(block=True, timeout=15)
         except Empty:
             return None
+#pylint: disable=W0221
 from http import HTTPStatus
 from http.client import HTTPConnection
 from http.server import BaseHTTPRequestHandler
@@ -60,18 +61,29 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Forward the request to app
         client = HTTPConnection('localhost', self.app_port)
         client.request(self.command, self.path)
+
+        # Obain the response
         response = client.getresponse()
 
         # Forward the response to client
         self.send_response(response.status)
+        for key, value in response.headers.items():
+            self.send_header(key, value, True)
         self.end_headers()
         self.wfile.write(response.read())
 
+    def send_header(self, key, value, it_is_us=False):
+        # We override this method with an additional flag. This is
+        # to avoid the Base class sending HTTP headers on our behalf.
+        # For instance "Server: BaseHTTP/0.6 Python/3.5.1".
+        if it_is_us:
+            super().send_header(key, value)
+
     def send_error(self, code, message=None, explain=None):
-        # This error will always occur upon receiving a request. To 
-        # support all HTTP verbs we ironically avoid any "DO_VERB" 
-        # methods in this class to trigger this error. We proceed
-        # by calling our "catch-all" handler called "DO_ALL".
+        # This error will always occur upon receiving a request. To
+        # support all HTTP verbs we ironically avoid any "DO_VERB"
+        # methods in this class to trigger this error. We sidestep
+        # the error handling by calling our "catch-all" method "DO_ALL".
         if code == HTTPStatus.NOT_IMPLEMENTED:
             self.do_ALL()
         else:
